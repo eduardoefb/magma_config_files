be620f745b1916daa37022319b16daee08b6927f
eduardoefb <eduardoefb@gmail.com>
2020-10-04 15:50:15 -0300

    first commit

--- sync_disks.sh
+++ sync_disks.sh
@@ -5,7 +5,8 @@ while :; do
       for node in `pcs status | grep --color -oP '(?<=Online: \[)(.*)(?=\])'`; do
          if [ "${node}" != "`echo ${HOSTNAME} | awk -F '.' '{print $1}'`" ]; then
             rsync -av -r --delete /srv/k8s_volume ${node}:/srv/
-            su - postgres -c "pg_dump ${POSTGRES_DB}" > /tmp/db_bkp
+            rsync -av -r --delete /var/lib/mysql ${node}:/var/lib
+            su - postgres -c "pg_dump ${POSTGRES_DB}" > /tmp/db_bkp            
             cat /tmp/db_bkp | psql -U postgres -h ${node} orc8r
             \rm -f /tmp/db_bkp 2>/dev/null          
          fi
